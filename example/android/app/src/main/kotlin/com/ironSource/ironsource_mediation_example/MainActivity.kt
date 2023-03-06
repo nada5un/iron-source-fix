080480cb235d76e23d2fb25379b7a88978bbfeed
daeun <sung980524@naver.com>
2023-03-06 17:43:44 +0900

    fix : FragmentActivity

--- example/android/app/src/main/kotlin/com/ironSource/ironsource_mediation_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/ironSource/ironsource_mediation_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.ironSource.ironsource_mediation_example
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
